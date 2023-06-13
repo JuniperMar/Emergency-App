6ef175249479ea7609ab146877d4ed35ec689fa6
JuniperMar <maliawilke@gmail.com>
2023-06-13 12:23:18 -0700

    Initial Commit

---
+++ Emergency App/Emergency_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emergency_AppApp.swift
+//  Emergency App
+//
+//  Created by Malia Wilke on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Emergency_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
